3302665f66fa95b308dd7cbf40fd1a9c852b24d6
Damon Ortega <dortega28@cnm.edu>
2023-02-27 07:42:51 -0700

    updated fjamming folder and files

--- fjamming/script.js
+++ fjamming/script.js
@@ -1,23 +1,6 @@
-
-$(document).ready(function(){
-    $('.your-class').slick({
-        setting-name: setting-value
-});
-});
-
-
-$('.slider-for').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    asNavFor: '.slider-nav'
-});
-$('.slider-nav').slick({
+$('.slider').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
-    asNavFor: '.slider-for',
-    dots: true,
-    centerMode: true,
-    focusOnSelect: true
+    autoplay: true,
+    autoplaySpeed: 2000,
 });
